5294270af6a44b2061762b29e90d485dd929558d
leeyuan <dbdks32123@gmail.com>
2023-11-09 19:24:57 +0900

    index 파일 GET 요청으로 응답

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 
 const port = 8080;
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "../public/index.html");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://127.0.0.1:${port}`);
 });
