7944b73b2d801e8c459eb1433f819099ae6b7ed5
leeyuan <dbdks32123@gmail.com>
2023-11-10 18:11:54 +0900

    데이터 POST 요청에 응답 - JSON 파일에 저장 안되는 문제

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mysql from 'mysql2';
+import fs from 'fs';
 
 const app = express();
 
@@ -15,9 +16,28 @@ const dbConfig = {
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "../public/index.html");
+  res.sendFile(__dirname + "./public/index.html");
 });
+
+app.post("/signup", (req, res) => {
+  const { name, password, email } = req.body;
+
+  fs.writeFile("./data/signUp.json", JSON.stringify({ name, password, email }, null, 2), (err) => {
+      if (err) {
+          console.error("Error writing data.json:", err);
+          res.status(500).send("Internal Server Error");
+      } else {
+          res.json({
+              status: "success",
+              formData: { name, password, email },
+          });
+      }
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://127.0.0.1:${port}`);
