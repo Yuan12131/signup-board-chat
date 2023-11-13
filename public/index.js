f7e783f6e2e605f065408539e08c12fa0318baae
leeyuan <dbdks32123@gmail.com>
2023-11-13 11:11:47 +0900

    fetch 함수 body stringfy 추가

--- public/index.js
+++ public/index.js
@@ -1,26 +1,32 @@
-const root = document.getElementById("root")
+const root = document.getElementById("root");
 const signupForm = document.getElementById("signupForm");
 
 const usernameOutput = document.createElement("div");
 root.appendChild(usernameOutput);
 
-signupForm.addEventListener("submit", async(event) => {
-    event.preventDefault();  // 폼의 기본 동작을 막습니다.
+signupForm.addEventListener("submit", async (event) => {
+    event.preventDefault(); // 폼의 기본 동작을 막음
     const formData = new FormData(signupForm);
 
     try {
         const response = await fetch("/signup", {
             method: "POST",
-            body: formData,
+            body: JSON.stringify(Object.fromEntries(formData)),
+            headers: {
+                "Content-Type": "application/json",
+            },
         });
-    
+
         if (response.ok) {
             const serverData = await response.json();
             usernameOutput.textContent = `가입한 이름: ${serverData.formData.name}`;
             signupForm.style.display = "none";
+        } else {
+            console.error("HTTP 오류:", response.status);
+            usernameOutput.textContent = "오류가 발생했습니다.";
         }
     } catch (error) {
         console.error("에러:", error);
-        usernameOutput.textContent = "오류가 발생했습니다.";
+        usernameOutput.textContent = "오류가 발생했습니다. 서버에 연결할 수 없습니다.";
     }
 });
