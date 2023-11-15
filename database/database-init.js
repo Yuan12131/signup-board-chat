3dd783af8c76a9c199802dbbc95c90b3bd4f06b1
leeyuan <dbdks32123@gmail.com>
2023-11-15 13:02:40 +0900

    명령어 ESM 방식으로 수정

--- database/database-init.js
+++ database/database-init.js
@@ -1,4 +1,9 @@
-// 데이터베이스의 테이블 생성 쿼리
+import fs from "fs/promises";
+import mysql from "mysql2/promise";
+import { dbConfig } from './config.mjs';
+
+const pool = mysql.createPool(dbConfig);
+
 const createTableQuery = `
   CREATE TABLE IF NOT EXISTS users (    
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -9,15 +14,15 @@ const createTableQuery = `
   );
 `;
 
-// Promise 기반으로 데이터베이스에 쿼리를 실행하고 그 결과를 반환
 async function initializeDatabase() {
   try {
-    // 연결 풀에서 연결을 가져와 createTableQuery 쿼리를 실행 -> 프로미스 반환
-    // await 키워드를 사용하여 프로미스가 완료될 때까지 대기 -> result에는 쿼리 실행 결과
     const [result] = await pool.query(createTableQuery);
+    console.log("Users table created successfully");
   } catch (err) {
     console.error("Error creating users table:", err);
   }
 }
 
-// ! DB 초기화 시 명령어 : node -e "require('./database/database-init.js').initializeDatabase()"
+export { initializeDatabase };
+
+// ! DB 초기화 시 명령어 : node -e "import('./database/database-init.js').then(module => module.initializeDatabase())"
