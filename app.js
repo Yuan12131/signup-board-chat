d68321b497cef064efbaa519439c2b5b79517110
leeyuan <dbdks32123@gmail.com>
2023-11-15 19:18:10 +0900

    DB 저장 함수 -> POST 요청으로 변경 후 route 처리

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 import fs from "fs/promises";
 import express from "express";
 import routes from "./route/routes.mjs";
-import {readJsonFile, insertRecords} from "./database/signup-database.mjs"
 
 const app = express();
 const port = 8080;
-
 // signUp.json 파일에서 데이터 읽어오기
-const jsonFilePath = "./data/signUp.json";
-
-// 서버 시작 함수
-async function startServer() {
-  try {
-    const jsonData = await readJsonFile(jsonFilePath);
-    const inputRecords = jsonData.inputRecords || [];
-
-    // 각 레코드를 비동기적으로 삽입
-    await insertRecords(inputRecords);
-  } catch (error) {
-    console.error("Error reading JSON file or inserting records:", error);
-  }
-}
-
-startServer();
 
 app.use(express.static("public"));
 app.use(express.json());
+
+
 
 app.use("/", routes);
 
