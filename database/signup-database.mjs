122ffbd963bfd7c0bc7aa06aefdbe6e631ce27c1
leeyuan <dbdks32123@gmail.com>
2023-11-15 19:17:22 +0900

    refactoring

--- database/signup-database.mjs
+++ database/signup-database.mjs
@@ -1,12 +1,12 @@
 import fs from "fs/promises";
 import mysql from "mysql2/promise"; // Promise 기반 MySQL2 모듈 사용
-import { dbConfig } from './config.mjs';
+import { dbConfig } from "./config.mjs";
 
 // MySQL Promise 기반 연결
 // createPool : MySQL 데이터베이스에 연결하기 위한 연결 풀을 생성
 // 연결 풀은 애플리케이션에서 데이터베이스에 연결할 때 이미 생성된 연결을 풀에서 가져와 사용함으로써 성능 향상
 const pool = mysql.createPool(dbConfig);
-
+const tableName = "users";
 
 // JSON 파일 읽기 함수
 async function readJsonFile(filePath) {
@@ -19,13 +19,11 @@ async function readJsonFile(filePath) {
   }
 }
 
-const tableName = "users";
-
 // 레코드 삽입 함수
 async function insertRecord(record) {
   try {
     const { name, password, email, timestamp } = record;
-  
+
     // 삽입 쿼리
     const insertQuery = `
       INSERT INTO ${tableName} (name, password, email, timestamp)
@@ -34,7 +32,6 @@ async function insertRecord(record) {
 
     // 삽입에 사용할 값들
     const values = [name, password, email, timestamp];
-
     // 쿼리 실행 및 결과 출력
     const [result] = await pool.query(insertQuery, values);
     return result;
@@ -50,9 +47,9 @@ async function insertRecords(records) {
     try {
       await insertRecord(record);
     } catch (error) {
-      console.error("Error inserting record:", error);
+      console.error("Error inserting records:", error);
     }
   }
 }
 
-export {readJsonFile, insertRecords}
+export { readJsonFile, insertRecords };
