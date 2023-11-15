cd2bf040c5deb8ce877dacdc3c64d33e0163fe3d
leeyuan <dbdks32123@gmail.com>
2023-11-15 19:34:08 +0900

    refactoring

--- route/routes.mjs
+++ route/routes.mjs
@@ -1,39 +1,40 @@
-import express from 'express';
+import express from "express";
 // 파일 시스템 관련 작업을 Promise 기반으로 처리할 수 있는 모듈 -> async/await와 함께 보다 간결한 비동기 코드 작성 가능
-import fs from 'fs/promises';
-import path from 'path'
+import fs from "fs/promises";
+import path from "path";
 import { readJsonFile, insertRecords } from "../database/signup-database.mjs";
 
 const router = express.Router();
-const jsonFilePath = new URL('../data/signUp.json', import.meta.url).pathname;
+const jsonFilePath = new URL("../data/signUp.json", import.meta.url).pathname;
 
 // GET 요청 처리
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // index.html 파일의 절대 경로
     // 현재 모듈을 기준으로 상대 경로를 해석하여 절대 경로로 변환
-    const indexPath = path.resolve(__dirname, './public/index.html');
+    const indexPath = path.resolve(__dirname, "./public/index.html");
     // fs/promises 모듈을 사용하여 파일을 읽어오는 비동기 코드
-    const indexData = await fs.readFile(indexPath, 'utf-8');
+    const indexData = await fs.readFile(indexPath, "utf-8");
     // 클라이언트에 읽은 파일 데이터를 응답
     res.send(indexData);
   } catch (error) {
-    console.error('Error reading index.html:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error reading index.html:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // '/signup' 경로에 대한 POST 요청 처리
-router.post('/signup', async (req, res) => {
-  
-  
+router.post("/signup", async (req, res) => {
   try {
     // POST 요청에서 속성 : name, password, email을 추출을 위한 비구조화 할당
     const { name, password, email } = req.body;
-     // timestamp를 MySQL datetime 형식('YYYY-MM-DD HH:mm:ss')으로 변환
-    const timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\.\d+Z$/, '');
+    // timestamp를 MySQL datetime 형식('YYYY-MM-DD HH:mm:ss')으로 변환
+    const timestamp = new Date()
+      .toISOString()
+      .replace(/T/, " ")
+      .replace(/\.\d+Z$/, "");
     // signUp.json 파일을 비동기적으로 가져오기
-    const data = await fs.readFile('./data/signUp.json', 'utf-8');
+    const data = await fs.readFile("./data/signUp.json", "utf-8");
     // JSON 데이터로 파싱
     const formData = JSON.parse(data);
 
@@ -49,7 +50,7 @@ router.post('/signup', async (req, res) => {
     formData.inputRecords.push(newRecord);
 
     // 업데이트된 JSON 데이터를 파일에 쓰기
-    await fs.writeFile('./data/signUp.json', JSON.stringify(formData, null, 2));
+    await fs.writeFile("./data/signUp.json", JSON.stringify(formData, null, 2));
 
     const jsonData = await readJsonFile(jsonFilePath);
     const inputRecords = jsonData.inputRecords || [];
@@ -58,7 +59,7 @@ router.post('/signup', async (req, res) => {
 
     // 성공적인 응답을 클라이언트에 전송
     res.json({
-      status: 'success',
+      status: "success",
       formData: {
         name: name,
         password: password,
@@ -66,8 +67,8 @@ router.post('/signup', async (req, res) => {
       },
     });
   } catch (error) {
-    console.error('Error handling signup:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error handling signup:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
