a542b49327fc42ad9c3d1a69f073100950adb729
leeyuan <dbdks32123@gmail.com>
2023-11-17 16:23:36 +0900

    boardForm POST test

--- route/routes.mjs
+++ route/routes.mjs
@@ -93,4 +93,68 @@ router.get("/chat.html", async (req, res) => {
   }
 });
 
+
+// GET 요청 처리
+router.get("/", async (req, res) => {
+  try {
+    // index.html 파일의 절대 경로
+    // 현재 모듈을 기준으로 상대 경로를 해석하여 절대 경로로 변환
+    const indexPath = path.resolve(__dirname, "./public/index.html");
+    // fs/promises 모듈을 사용하여 파일을 읽어오는 비동기 코드
+    const indexData = await fs.readFile(indexPath, "utf-8");
+    // 클라이언트에 읽은 파일 데이터를 응답
+    res.send(indexData);
+  } catch (error) {
+    console.error("Error reading index.html:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// '/board' 경로에 대한 POST 요청 처리
+router.post("/board", async (req, res) => {
+  try {
+    // POST 요청에서 속성 : name, password, email을 추출을 위한 비구조화 할당
+    const { title, content } = req.body;
+    // timestamp를 MySQL datetime 형식('YYYY-MM-DD HH:mm:ss')으로 변환
+    const timestamp = new Date().toISOString().replace(/T/, " ").replace(/\.\d+Z$/, "");
+    // signUp.json 파일을 비동기적으로 가져오기
+    const data = await fs.readFile("./data/board.json", "utf-8");
+    // JSON 데이터로 파싱
+    const formData = JSON.parse(data);
+
+    // 새로운 레코드 객체를 생성
+    const newRecord = {
+      name: name,
+      title: title,
+      content: content,
+      timestamp: timestamp,
+    };
+
+    // 기존 데이터 배열에 새 레코드를 추가
+    formData.inputRecords.push(newRecord);
+
+    // 업데이트된 JSON 데이터를 파일에 쓰기
+    await fs.writeFile("./data/board.json", JSON.stringify(formData, null, 2));
+
+    const jsonData = await readJsonFile(jsonFilePath);
+    const inputRecords = jsonData.inputRecords || [];
+
+    await insertRecords(inputRecords);
+
+    // 성공 응답 클라이언트에 전송
+    res.json({
+      status: "success",
+      formData: {
+        name: name,
+        title: title,
+        content: content,
+        timestamp: timestamp,
+      },
+    });
+  } catch (error) {
+    console.error("Error handling signup:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 export default router;
