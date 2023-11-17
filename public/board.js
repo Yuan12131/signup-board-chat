0d9108cf020a01c2beea0e285ca3a588c0e46962
leeyuan <dbdks32123@gmail.com>
2023-11-17 14:34:36 +0900

    list 클릭 시 글 내용 출력 모달 추가

--- public/board.js
+++ public/board.js
@@ -1,15 +1,25 @@
-// JavaScript로 모달을 제어하는 스크립트
-const openWriteModal = document.getElementById('openWriteModal');
-const closeWriteModal = document.getElementById('closeWriteModal');
-const writeModal = document.getElementById('writeModal');
-const overlay = document.getElementById('overlay');
+const openWriteModal = document.getElementById("openWriteModal");
+const closeWriteModal = document.getElementById("closeWriteModal");
+const writeModal = document.getElementById("writeModal");
+const overlay = document.getElementById("overlay");
 
-openWriteModal.addEventListener('click', () => {
-  writeModal.style.display = 'block';
-  overlay.style.display = 'block';
+openWriteModal.addEventListener("click", () => {
+  writeModal.style.display = "block";
+  overlay.style.display = "block";
 });
 
-closeWriteModal.addEventListener('click', () => {
-  writeModal.style.display = 'none';
-  overlay.style.display = 'none';
+closeWriteModal.addEventListener("click", () => {
+  writeModal.style.display = "none";
+  overlay.style.display = "none";
+});
+
+// 각각의 글 제목 클릭 시 해당 글의 내용을 모달에 출력
+const itemRows = document.querySelectorAll(".item-row");
+itemRows.forEach((row) => {
+  row.addEventListener("click", () => {
+    // 여기서 서버에 해당 글의 내용을 요청하고, 응답으로 받은 데이터를 모달에 채워 넣는 로직을 추가
+    const content = row.dataset.content;
+    // 예시로 받은 데이터를 모달에 표시
+    alert(content);
+  });
 });
