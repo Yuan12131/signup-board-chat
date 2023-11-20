717ce9c74d4e226049d6e71d643ac3da835c8d8f
leeyuan <dbdks32123@gmail.com>
2023-11-20 19:06:23 +0900

    로그인, 로그아웃 함수 수정

--- public/index.js
+++ public/index.js
@@ -4,29 +4,54 @@ const signupBtn = document.getElementById("signupBtn");
 const closeSignupModal = document.getElementById("closeSignupModal");
 const signupModal = document.getElementById("signupModal");
 const indexOverlay = document.getElementById("indexOverlay");
-const usernameOutput = document.createElement("div");
-root.appendChild(usernameOutput);
 const loginBtn = document.getElementById("loginBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 const loginForm = document.getElementById("loginForm");
 const loggedInUserId = document.getElementById("loggedInUserId");
+const h3 = document.querySelector("h3");
 
 // 로그인 함수
-loginBtn.addEventListener("click", function () {
-  const userId = document.getElementById("userId").value;
-  // 여기에서 로그인 로직을 처리할 수 있습니다.
-  // 성공 시 ID 출력 창을 갱신하고 로그인 및 회원가입 버튼 상태를 변경합니다.
-  loggedInUserId.innerText = userId + "님 반갑습니다!";
-  loggedInUserId.style.display = "block";
-  loginForm.style.display = "none";
-  logoutBtn.style.display = "block";
-  signupBtn.style.display = "none";
+loginBtn.addEventListener("click", async function () {
+  const userId = document.getElementById('userId').value;
+  const userPassword = document.getElementById('userPassword').value;
+
+  try {
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        signupId: userId,
+        signupPassword: userPassword,
+      }),
+    });
+
+    const result = await response.text();
+
+    if (response.ok) {
+      // 로그인 성공 시
+      console.log('Login successful');
+      h3.innerText = userId + "님 반갑습니다!";
+      loggedInUserId.style.display = "block";
+      loginForm.style.display = "none";
+      logoutBtn.style.display = "block";
+      signupBtn.style.display = "none";
+    } else {
+      // 로그인 실패 시
+      console.error('Login failed:', result);
+      alert("올바른 ID와 PASSWORD를 입력해주세요")
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+    alert("로그인 중 오류가 발생했습니다.");
+  }
 });
 
 // 로그아웃 함수
 logoutBtn.addEventListener("click", function () {
-  // 여기에서 로그아웃 로직을 처리할 수 있습니다.
-  // 로그아웃 시 ID 출력 창을 갱신하고 로그인 및 회원가입 버튼 상태를 변경합니다.
+  // 로그아웃 시 초기화
+  h3.innerText = "로그인 후 서비스를 이용하세요"
   loggedInUserId.style.display = "none";
   loginForm.style.display = "block";
   logoutBtn.style.display = "none";
@@ -39,10 +64,13 @@ signupBtn.addEventListener("click", () => {
   indexOverlay.style.display = "block";
   addSubmitEventListener();
 });
+
+// 모달창 닫기 버튼 이벤트 리스너
 closeSignupModal.addEventListener("click", () => {
   signupModal.style.display = "none";
   indexOverlay.style.display = "none";
 });
+
 // 회원가입 폼 데이터 처리 로직
 function addSubmitEventListener() {
   if (signupForm) {
@@ -60,11 +88,13 @@ function addSubmitEventListener() {
         });
 
         if (response.ok) {
+          // 회원가입 성공 시
           const serverData = await response.json();
-          usernameOutput.textContent = `${serverData.formData.signupId}님, 가입에 성공했습니다!`;
+          h3.textContent = `${serverData.formData.signupId}님, 가입에 성공했습니다!`;
           signupModal.style.display = "none";
           indexOverlay.style.display = "none";
 
+          // 이동할 페이지 설정
           const boardLink = document.querySelector(".board");
           boardLink.addEventListener("click", function () {
             window.location.href = "/board.html";
@@ -75,13 +105,14 @@ function addSubmitEventListener() {
             window.location.href = "/chat.html";
           });
         } else {
+          // 회원가입 실패 시
           console.error("HTTP 오류:", response.status);
-          usernameOutput.textContent = "오류가 발생했습니다.";
+          h3.textContent = "오류가 발생했습니다.";
         }
       } catch (error) {
+        // 오류 발생 시
         console.error("에러:", error);
-        usernameOutput.textContent =
-          "오류가 발생했습니다. 서버에 연결할 수 없습니다.";
+        h3.textContent = "오류가 발생했습니다. 서버에 연결할 수 없습니다.";
       }
     });
   } else {
