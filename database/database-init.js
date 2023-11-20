d3c44162da33682e27ed1a0e4311287c6570522a
leeyuan <dbdks32123@gmail.com>
2023-11-20 18:24:08 +0900

    테이블 쿼리 id, name 변경

--- database/database-init.js
+++ database/database-init.js
@@ -1,17 +1,17 @@
 import mysql from "mysql2/promise";
-import { dbConfig } from './config.mjs';
+import { dbConfig } from "./config.mjs";
 
 const pool = mysql.createPool(dbConfig);
 
 // 테이블 쿼리
 const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS users (    
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  );
+CREATE TABLE IF NOT EXISTS users (    
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  signupId VARCHAR(255) NOT NULL,
+  signupPassword VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 `;
 
 // 데이터베이스 초기화 함수
