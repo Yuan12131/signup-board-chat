1080d6824aabff12971df3222415ceb75dcf25fc
leeyuan <dbdks32123@gmail.com>
2023-11-21 20:29:39 +0900

    board table query 수정

--- database/database-init.js
+++ database/database-init.js
@@ -4,29 +4,33 @@ import { dbConfig } from "./config.mjs";
 const pool = mysql.createPool(dbConfig);
 
 // 테이블 쿼리
-const createUsersTableQuery = `CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  signupId VARCHAR(255) NOT NULL,
-  signupPassword VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-`
+const createUsersTableQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    signupId VARCHAR(255) NOT NULL,
+    signupPassword VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    UNIQUE KEY unique_signupId (signupId)
+  );
+`;
 
-const createBoardTableQuery = `CREATE TABLE IF NOT EXISTS board (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  content TEXT NOT NULL,
-  userId INT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (userId) REFERENCES users(id)
-);
-`
+const createBoardTableQuery = `
+  CREATE TABLE IF NOT EXISTS board (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    userId VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users(signupId)
+  );
+`;
 
 // 데이터베이스 초기화 함수
 async function initializeDatabase() {
   try {
-    const [result] = await pool.query(createBoardTableQuery);
+    const [result1] = await pool.query(createUsersTableQuery);
+    const [result2] = await pool.query(createBoardTableQuery);
     console.log("table created successfully");
   } catch (err) {
     console.error("Error creating users table:", err);
