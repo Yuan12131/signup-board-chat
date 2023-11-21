acf5834569ea78bfde21d6561c7995a9a798e041
leeyuan <dbdks32123@gmail.com>
2023-11-21 15:01:00 +0900

    loginsession 분리

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 import express from "express";
 import routes from "./route/routes.mjs";
-import session from 'express-session';
 
 const app = express();
 const port = 8080;
-
-// 세션 설정
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-}));
 
 // 정적 파일 미들웨어 등록
 app.use(express.static("public"));
