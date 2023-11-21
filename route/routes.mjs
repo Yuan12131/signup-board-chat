dc7c00dc08619a89d80b7c40c161ccd6b74dcfde
leeyuan <dbdks32123@gmail.com>
2023-11-21 17:12:02 +0900

    id 중복확인 추가

--- route/routes.mjs
+++ route/routes.mjs
@@ -1,30 +1,36 @@
 import express from "express";
 import fs from "fs/promises"; // Promise 기반의 fs 모듈
 import path from "path";
-import { readJsonFile, insertRecords, pool } from "../database/signup-database.mjs";
-import session from 'express-session';
+import {
+  readJsonFile,
+  insertRecords,
+  pool,
+} from "../database/signup-database.mjs";
+import session from "express-session";
 
 const router = express.Router();
 const jsonFilePath = new URL("../data/signUp.json", import.meta.url).pathname;
 
 // 세션 설정
-router.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-}));
+router.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 // 로그인 세션 확인
-router.get('/check-session', (req, res) => {
+router.get("/check-session", (req, res) => {
   if (req.session && req.session.user) {
     // 세션이 있는 경우, 세션은 유효하다고 응답합니다.
-    res.status(200).send('세션 유효');
+    res.status(200).send("세션 유효");
   } else {
     // 세션이 없는 경우, 세션이 유효하지 않다고 응답합니다.
-    res.status(401).send('세션 유효하지 않음');
+    res.status(401).send("세션 유효하지 않음");
   }
 });
 
@@ -44,45 +50,46 @@ router.get("/", async (req, res) => {
 });
 
 // login 요청
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { signupId, signupPassword } = req.body;
-    
+
     // 로그인 로직 수행
     // SQL 쿼리를 정의 - users 테이블에서 주어진 signupId와 signupPassword와 일치하는 레코드를 찾음
-    const selectQuery = 'SELECT * FROM users WHERE signupId = ? AND signupPassword = ?';
+    const selectQuery =
+      "SELECT * FROM users WHERE signupId = ? AND signupPassword = ?";
     // await pool.query(selectQuery, [signupId, signupPassword])를 사용하여 데이터베이스에서 쿼리를 실행
     // pool.query는 MySQL 풀에서 연결을 가져와 쿼리를 실행하며, [signupId, signupPassword]는 SQL 쿼리에 전달할 매개변수
     // 쿼리 결과를 분해하여 첫 번째 요소를 results에 할당 - results는 SQL 쿼리에 의해 반환된 결과
     const [results] = await pool.query(selectQuery, [signupId, signupPassword]);
-    
+
     // 쿼리 결과가 하나 이상의 레코드를 반환했는지 확인 -> 사용자가 올바른 아이디와 비밀번호로 로그인한 경우
     if (results.length > 0) {
       // 로그인 성공
       // 로그인이 성공한 경우, req.session.user에 사용자 정보를 저장 -> 세션에 사용자 정보를 기록
       req.session.user = { id: signupId };
-      console.log('User logged in:', req.session.user);
-      res.status(200).send('Login successful');
+      console.log("User logged in:", req.session.user);
+      res.status(200).send("Login successful");
     } else {
       // 로그인 실패
-      res.status(401).send('Invalid login credentials');
+      res.status(401).send("Invalid login credentials");
     }
   } catch (error) {
-    console.error('Error during login:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error during login:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // logout 요청
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      console.error('Error destroying session:', err);
-      res.status(500).send('Internal Server Error');
+      console.error("Error destroying session:", err);
+      res.status(500).send("Internal Server Error");
     } else {
       // 클라이언트에게 응답을 보내기 전에 세션 쿠키를 클리어
-      res.clearCookie('connect.sid');
-      res.status(200).send('Logout successful');
+      res.clearCookie("connect.sid");
+      res.status(200).send("Logout successful");
     }
   });
 });
@@ -93,12 +100,15 @@ router.post("/signup", async (req, res) => {
     // POST 요청에서 속성 : id, password, email을 추출을 위한 비구조화 할당
     const { signupId, signupPassword, email } = req.body;
     // timestamp를 MySQL datetime 형식('YYYY-MM-DD HH:mm:ss')으로 변환
-    const timestamp = new Date().toISOString().replace(/T/, " ").replace(/\.\d+Z$/, "");
+    const timestamp = new Date()
+      .toISOString()
+      .replace(/T/, " ")
+      .replace(/\.\d+Z$/, "");
     // signUp.json 파일을 비동기적으로 가져오기
     const data = await fs.readFile("./data/signUp.json", "utf-8");
     // JSON 데이터로 파싱
     const formData = JSON.parse(data);
-    
+
     // 새로운 레코드 객체를 생성
     const newRecord = {
       signupId: signupId,
@@ -106,18 +116,18 @@ router.post("/signup", async (req, res) => {
       email: email,
       timestamp: timestamp,
     };
-    
+
     // 기존 데이터 배열에 새 레코드를 추가
     formData.inputRecords.push(newRecord);
-    
+
     // 업데이트된 JSON 데이터를 파일에 쓰기
     await fs.writeFile("./data/signUp.json", JSON.stringify(formData, null, 2));
-    
+
     const jsonData = await readJsonFile(jsonFilePath);
     const inputRecords = jsonData.inputRecords || [];
-    
+
     await insertRecords(inputRecords);
-    
+
     // 성공 응답 클라이언트에 전송
     res.json({
       status: "success",
@@ -133,18 +143,48 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/check-id-duplicate", async (req, res) => {
+  try {
+    const { signupId } = req.body;
+
+    // 중복 여부 확인 함수 호출
+    const isIdDuplicate = await checkIdDuplicate(signupId);
+
+    // 클라이언트에 응답
+    res.json({ isIdDuplicate });
+  } catch (error) {
+    console.error("Error checking ID duplicate:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+async function checkIdDuplicate(signupId) {
+  // 여기에서는 가정일 뿐이며, 실제로는 데이터베이스에서 아이디를 검색하여 중복 여부를 판단해야 합니다.
+  // MySQL을 사용한다고 가정하고, 실제 코드는 사용하는 데이터베이스에 따라 다를 수 있습니다.
+  const selectQuery = "SELECT COUNT(*) as count FROM users WHERE signupId = ?";
+  const [result] = await pool.query(selectQuery, [signupId]);
+
+  // 결과에서 중복 여부 확인
+  const isDuplicate = result[0].count > 0;
+
+  return isDuplicate;
+}
+
 // '/board' 경로에 대한 POST 요청 처리
 router.post("/board", async (req, res) => {
   try {
     // POST 요청에서 속성 : name, password, email을 추출을 위한 비구조화 할당
     const { title, content } = req.body;
     // timestamp를 MySQL datetime 형식('YYYY-MM-DD HH:mm:ss')으로 변환
-    const timestamp = new Date().toISOString().replace(/T/, " ").replace(/\.\d+Z$/, "");
+    const timestamp = new Date()
+      .toISOString()
+      .replace(/T/, " ")
+      .replace(/\.\d+Z$/, "");
     // signUp.json 파일을 비동기적으로 가져오기
     const data = await fs.readFile("./data/board.json", "utf-8");
     // JSON 데이터로 파싱
     const formData = JSON.parse(data);
-    
+
     // 새로운 레코드 객체를 생성
     const newRecord = {
       id: signupId,
@@ -152,18 +192,18 @@ router.post("/board", async (req, res) => {
       content: content,
       timestamp: timestamp,
     };
-    
+
     // 기존 데이터 배열에 새 레코드를 추가
     formData.inputRecords.push(newRecord);
-    
+
     // 업데이트된 JSON 데이터를 파일에 쓰기
     await fs.writeFile("./data/board.json", JSON.stringify(formData, null, 2));
-    
+
     const jsonData = await readJsonFile(jsonFilePath);
     const inputRecords = jsonData.inputRecords || [];
-    
+
     await insertRecords(inputRecords);
-    
+
     // 성공 응답 클라이언트에 전송
     res.json({
       status: "success",
