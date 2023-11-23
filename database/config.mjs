8fffdd24554e8c77956b7139cc0338c6c699d931
leeyuan <dbdks32123@gmail.com>
2023-11-23 16:04:49 +0900

    env 삭제

--- database/config.mjs
+++ database/config.mjs
@@ -1,15 +1,12 @@
-import dotenv from 'dotenv';
 import mysql2 from "mysql2/promise"
-
-dotenv.config();
 
 const dbConfig = {
   host: "localhost",
   user: "lee",
-  password: process.env.DB_PASSWORD,
+  password: "kdt",
   database: "community",
 };
 
 const pool = mysql2.createPool(dbConfig);
 
-export { dbConfig, pool };
+export { pool };
