77880578005b9c87159fe6959e5e8251c88f8abd
leeyuan <dbdks32123@gmail.com>
2023-11-24 21:04:26 +0900

    socket 연결시 채팅방 목록 받기

--- public/chat.js
+++ public/chat.js
@@ -1,30 +1,17 @@
-const socket = io('http://localhost:8000');
-const roomList = document.getElementById("roomList");
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = document.getElementById('m').value;
-    socket.emit('chat message', message);
-    document.getElementById('m').value = '';
-});
+const socket = io('http://localhost:8000', { path: '/socket.io' });
 
-socket.on('chat message', (msg) => {
-    const ul = document.getElementById('messages');
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(msg));
-    ul.appendChild(li);
-});
+const chatRoom = document.getElementById('roomList');
 
-// 방 목록을 받아서 처리
-socket.on("roomList", (rooms) => {
-
+socket.on('roomList', (rooms) => {
+  console.log(rooms)
+  // 받은 방 목록을 활용하여 UI에 표시
   rooms.forEach((room) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = room.name;
-    listItem.addEventListener("click", () => {
-      // 클릭한 방에 입장하는 로직 추가
-      console.log(`Joining room: ${room.name}`);
-    });
-
-    roomList.appendChild(listItem);
+    const roomItem = document.createElement('li');
+    roomItem.textContent = `Room ${room.id}: ${room.roomId} (Host: ${room.hostId})`;
+    chatRoom.appendChild(roomItem);
   });
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from the server');
 });
