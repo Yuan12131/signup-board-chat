e1f444960b0fead4c61bf410ebaedb17fad5dde8
leeyuan <dbdks32123@gmail.com>
2023-11-24 21:05:30 +0900

    socket 분리

--- app.js
+++ app.js
@@ -1,53 +1,29 @@
 import express from "express";
-import routes from "./route/routes.mjs";
-import { createServer } from 'http';
+import { createServer } from 'http';  // http 모듈 추가
+import { router, attachSocketEvents } from "./route/routes.mjs";
 import { Server } from 'socket.io';
+import cors from 'cors';
+
 const app = express();
+const server = createServer(app);  // createServer 함수 사용
+const io = new Server(server, { path: '/socket.io' });
 const PORT = 8000;
-const server = createServer(app);
-const io = new Server(server);
 
 // 정적 파일 미들웨어 등록
 app.use(express.static("public"));
 
-// 라우트 등록
-app.use("/", routes);
+app.use(cors());
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port : http://localhost:${PORT}`);
-});
+// 라우트 등록
+app.use("/", router);
 
 // Socket.io 이벤트 리스너 추가
-io.on('connection', (socket) => {
-  console.log('A user connected');
+attachSocketEvents(io); // attachSocketEvents 함수에 io를 전달하도록 수정
 
-  // 예시: 클라이언트로 메시지 전송
-  socket.emit('message', 'Welcome to the chat!');
-
-  // 클라이언트로 방 목록 전송
-  socket.emit("roomList", rooms);
-
-  // 클라이언트로부터 메시지 수신
-  socket.on('chat message', (message) => {
-      console.log(`Received message: ${message}`);
-      // 예시: 모든 클라이언트에게 메시지 전송
-      io.emit('chat message', message);
-  });
-
-  // 연결이 끊어졌을 때 처리
-  socket.on('disconnect', () => {
-      console.log('A user disconnected');
-  });
-});
-
-// 방 정보를 저장할 배열
-const rooms = [
-  { id: 1, hostId: "vv", name: "Room 1" },
-  { id: 2, hostId: "sh", name: "Room 2" },
-  { id: 3, hostId: "hello", name: "Room 3" },
-];
-
-// 방 목록을 클라이언트에게 전달
-app.get("/rooms", (req, res) => {
-  res.json(rooms);
+server.listen(PORT, (err) => {
+  if (err) {
+    console.error('Server failed to start:', err);
+  } else {
+    console.log(`Server is running on port: http://localhost:${PORT}`);
+  }
 });
