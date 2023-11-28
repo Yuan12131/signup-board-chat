4e214edaacb1e3d69b80d30d5e36678f8289d075
leeyuan <dbdks32123@gmail.com>
2023-11-28 19:13:23 +0900

    refactoring

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
-import cors from "cors";
+import { createServer } from 'http';  // http 모듈 추가
+import { Server } from 'socket.io';
 import session from "express-session";
-import { createServer } from "http";
-import { Server } from "socket.io";
+import cors from 'cors';
+import { pool } from "./database/config.mjs";
 import { router, attachSocketEvents } from "./route/routes.mjs";
 
 const app = express();
@@ -10,13 +11,17 @@ const app = express();
 // Socket.io는 HTTP 서버를 사용하여 웹소켓 연결을 설정 (HTTP 서버 위에서 작동)
 // createServer(app)를 사용하여 만든 HTTP 서버 객체를 이용해 소켓 연결을 활성화
 const server = createServer(app);
+
 // Socket.io의 Server 클래스를 사용하여 웹소켓 서버를 생성
-// ! io 객체는 웹소켓 연결을 관리하고, 클라이언트와 서버 간의 양방향 통신을 담당
+// io 객체는 웹소켓 연결을 관리하고, 클라이언트와 서버 간의 양방향 통신을 담당
 const io = new Server(server, { path: "/socket.io" });
+
 const PORT = 8000;
 
+// Express 애플리케이션에서 사용되는 세션 관리를 위한 미들웨어를 설정
+// 세션은 사용자의 상태를 서버에 저장하고 유지하는 데 사용되며, 로그인 상태를 유지하거나 사용자에 대한 정보를 저장하는 데 주로 활용
 const sessionMiddleware = session({
-  secret: "your-secret-key",
+  secret: "kdt",
   resave: false,
   saveUninitialized: false,
   store: new session.MemoryStore(),
@@ -39,6 +44,17 @@ attachSocketEvents(io);
 io.use((socket, next) => {
   // 요청 객체, 응답 객체 (기본값)
   sessionMiddleware(socket.request, socket.request.res || {}, next);
+});
+
+// 프로그램 종료 시 MySQL 연결 종료
+process.on("SIGINT", async () => {
+  if (pool._closed) {
+    console.log("MySQL connection is already closed.");
+  } else {
+    await pool.end();
+    console.log("MySQL connection closed.");
+  }
+  process.exit();
 });
 
 server.listen(PORT, (err) => {
