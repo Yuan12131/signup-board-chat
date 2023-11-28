b4bd43b1ec9662b613ceb4bea265a252ebb65969
leeyuan <dbdks32123@gmail.com>
2023-11-28 19:15:03 +0900

    refactoring

--- route/routes.mjs
+++ route/routes.mjs
@@ -1,20 +1,15 @@
 import express from "express";
 import fs from "fs/promises"; // Promise 기반의 fs 모듈
 import path from "path";
-import session from "express-session";
-import multer from "multer";
+import multer from "multer"; // Node.js에서 파일 업로드를 처리하기 위한 미들웨어
+import { pool } from "../database/config.mjs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-import { pool } from "../database/config.mjs";
 import { readJsonFile } from "../database/readJsonFile.mjs";
 import { insertRecords } from "../database/signup-database.mjs";
 import { insertBoardRecords } from "../database/board-database.mjs";
+import { insertRoomsRecord, insertMessagesRecord, loadMessages } from "../database/chat-database.mjs";
 
-import { Server } from "socket.io";
-import { createServer } from "http";
-
-const app = express();
-const server = createServer(app);
 const router = express.Router();
 const signupJsonFile = new URL("../data/signUp.json", import.meta.url).pathname;
 const boardJsonFile = new URL("../data/board.json", import.meta.url).pathname;
@@ -26,13 +21,14 @@ router.use(express.urlencoded({ extended: true }));
 // 예시: 이미지가 저장된 디렉토리를 정적 파일로 제공하는 설정
 router.use("/data/uploadsImg", express.static("data/uploadsImg"));
 
+// 세션은 사용자가 로그인한 후 서버에 의해 유지되는 데이터 저장 공간
 // 로그인 세션 확인
 router.get("/check-session", (req, res) => {
   if (req.session && req.session.user) {
-    // 세션이 있는 경우, 세션은 유효하다고 응답합니다.
+    // 세션이 있는 경우, 세션은 유효하다고 응답
     res.status(200).send("세션 유효");
   } else {
-    // 세션이 없는 경우, 세션이 유효하지 않다고 응답합니다.
+    // 세션이 없는 경우, 세션이 유효하지 않다고 응답
     res.status(401).send("세션 유효하지 않음");
   }
 });
@@ -56,7 +52,6 @@ router.get("/", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { signupId, signupPassword } = req.body;
-
     // 로그인 로직 수행
     // SQL 쿼리를 정의 - users 테이블에서 주어진 signupId와 signupPassword와 일치하는 레코드를 찾음
     const selectQuery =
@@ -68,7 +63,6 @@ router.post("/login", async (req, res) => {
 
     // 쿼리 결과가 하나 이상의 레코드를 반환했는지 확인 -> 사용자가 올바른 아이디와 비밀번호로 로그인한 경우
     if (results.length > 0) {
-      // 로그인 성공
       // 로그인이 성공한 경우, req.session.user에 사용자 정보를 저장 -> 세션에 사용자 정보를 기록
       req.session.user = { id: signupId };
       console.log("User logged in:", req.session.user);
@@ -85,6 +79,7 @@ router.post("/login", async (req, res) => {
 
 // logout 요청
 router.get("/logout", (req, res) => {
+  // 현재 세션을 파괴하는 메서드
   req.session.destroy((err) => {
     if (err) {
       console.error("Error destroying session:", err);
@@ -127,6 +122,7 @@ router.post("/signup", async (req, res) => {
     await fs.writeFile("./data/signUp.json", JSON.stringify(formData, null, 2));
 
     const jsonData = await readJsonFile(signupJsonFile);
+    // jsonData에 inputRecords가 존재하지 않을 경우에는 빈 배열([])
     const inputRecords = jsonData.inputRecords || [];
 
     await insertRecords(inputRecords);
@@ -146,6 +142,18 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+async function checkIdDuplicate(signupId) {
+  const selectQuery = "SELECT COUNT(*) as count FROM users WHERE signupId = ?";
+  // 비구조화 할당
+  // pool.query 함수의 결과로 반환되는 배열의 첫 번째 요소를 result 변수에 할당
+  // selectQuery로 정의된 쿼리를 실행하고, 그 중에서 signupId 값이 일치하는 데이터를 검색하는 역할
+  const [result] = await pool.query(selectQuery, [signupId]);
+
+  // 결과에서 중복 여부 확인
+  const isDuplicate = result[0].count > 0;
+  return isDuplicate;
+}
+
 router.post("/check-id-duplicate", async (req, res) => {
   try {
     const { signupId } = req.body;
@@ -161,17 +169,9 @@ router.post("/check-id-duplicate", async (req, res) => {
   }
 });
 
-async function checkIdDuplicate(signupId) {
-  const selectQuery = "SELECT COUNT(*) as count FROM users WHERE signupId = ?";
-  const [result] = await pool.query(selectQuery, [signupId]);
-
-  // 결과에서 중복 여부 확인
-  const isDuplicate = result[0].count > 0;
-  return isDuplicate;
-}
-
 // 이미지를 저장할 디렉토리 설정
 const storage = multer.diskStorage({
+  // 이미지 파일이 저장될 위치와 파일명을 지정
   destination: function (req, file, cb) {
     cb(null, "./data/uploadsImg"); // 'uploads/' 디렉토리에 이미지를 저장
   },
@@ -180,6 +180,7 @@ const storage = multer.diskStorage({
   },
 });
 
+// multer를 호출해서 파일 업로드를 처리하고 저장할 위치 및 파일명에 대한 설정이 담긴 storage 객체를 받는 미들웨어 설정
 const upload = multer({ storage: storage });
 
 // '/board' 경로에 대한 POST 요청 처리
@@ -188,6 +189,7 @@ router.post("/board", upload.single("image"), async (req, res) => {
     const { title, content } = req.body;
 
     const userId = req.session.user.id; // 현재 로그인된 사용자의 ID
+    
     // timestamp를 MySQL datetime 형식('YYYY-MM-DD HH:mm:ss')으로 변환
     const timestamp = new Date()
       .toISOString()
@@ -257,69 +259,6 @@ router.get("/get-posts", async (req, res) => {
   }
 });
 
-async function insertRoomsRecord(record) {
-  try {
-    const { roomId, userId, isHost } = record;
-
-    const timestamp = new Date()
-      .toISOString()
-      .replace("T", " ")
-      .replace("Z", "");
-
-    const insertQuery = `
-  INSERT INTO rooms (roomId, userId, isHost, timestamp)
-  VALUES (?, ?, ?, ?);
-`;
-
-    const values = [roomId, userId, isHost, timestamp];
-    const [result] = await pool.query(insertQuery, values);
-    return result;
-  } catch (err) {
-    console.error("rooms 테이블에 데이터를 삽입하는 중 오류 발생:", err);
-    throw err;
-  }
-}
-
-async function insertMessagesRecord(record) {
-  try {
-    const { roomId, userId, isHost, message } = record;
-
-    const timestamp = new Date()
-      .toISOString()
-      .replace("T", " ")
-      .replace("Z", "");
-
-    const insertQuery = `
-  INSERT INTO messages (roomId, userId, isHost, message, timestamp)
-  VALUES (?, ?, ?, ?, ?);
-`;
-
-    const values = [roomId, userId, isHost, message, timestamp];
-    const [result] = await pool.query(insertQuery, values);
-    return result;
-  } catch (err) {
-    console.error("messages 테이블에 데이터를 삽입하는 중 오류 발생:", err);
-    throw err;
-  }
-}
-
-async function loadMessages(roomId) {
-  try {
-    const messages = await getMessagesByRoomId(roomId);
-    return messages;
-  } catch (error) {
-    console.error("Error loading messages:", error);
-    throw error;
-  }
-}
-
-async function getMessagesByRoomId(roomId) {
-  const selectQuery =
-    "SELECT * FROM messages WHERE roomId = ? ORDER BY timestamp;";
-  const [messages] = await pool.query(selectQuery, [roomId]);
-  return messages;
-}
-
 const attachSocketEvents = (io) => {
   // 방 목록
   const rooms = [
@@ -331,37 +270,38 @@ const attachSocketEvents = (io) => {
   io.on("connection", (socket) => {
     // 초기 방 목록을 클라이언트에게 보내줌
     socket.emit("roomList", rooms);
-  
+
     // 클라이언트가 userIdRequest 이벤트를 발생시키면 사용자 ID를 전송
-    socket.on('userIdRequest', () => {
-      const userId = socket.request.session.user.id;
-      socket.emit('userId', { userId });
+    socket.on("userIdRequest", () => {
+      const userId = socket.request.session.user;
+      socket.emit("userId", { userId });
     });
 
-    // 방 입장 처리
+    // 방 입장 시 클라이언트에서 "joinRoom" 이벤트가 발생하면 이 이벤트를 처리하는 콜백 함수가 실행
     socket.on("joinRoom", async (data) => {
-      const { roomId } = data;
+      const { roomId } = data; // 클라이언트에서 전달된 데이터 객체에서 roomId를 추출
       const userId = socket.request.session.user.id;
-  
+      socket.join(roomId);
+
       try {
-        // 여기에서 DB 조회를 통해 방의 정보를 확인하고, 호스트 여부를 판단하여 결과를 반환
         const roomInfo = rooms.find((room) => room.roomId === roomId);
-  
+
         if (roomInfo) {
-          // 호스트 여부에 따라 isHost 값 설정
           const isHost = roomInfo.hostId === userId;
-  
+
           // DB에 입장 정보 저장
-          insertRoomsRecord({
+          await insertRoomsRecord({
             roomId: roomId,
             userId: userId,
             isHost: isHost,
             timestamp: new Date().toISOString(),
           });
-  
+
+          // DB에서 해당 방의 이전 메시지를 불러옴
+          const messages = await loadMessages(roomId);
+          socket.emit("loadMessages", messages); // 클라이언트에 전송
         } else {
           // 방이 존재하지 않는 경우 등에 대한 처리
-          // 클라이언트에 에러 메시지 등을 전송
           socket.emit("joinRoomError", { message: "Invalid room or user" });
         }
       } catch (error) {
@@ -369,17 +309,17 @@ const attachSocketEvents = (io) => {
         socket.emit("joinRoomError", { message: "User authentication failed" });
       }
     });
-  
+
     socket.on("sendMessage", async (data) => {
       const { message, roomId } = data;
       const userId = socket.request.session.user.id;
-  
+
       try {
         const roomInfo = rooms.find((room) => room.roomId === roomId);
-  
+
         if (roomInfo) {
           const isHost = roomInfo.hostId === userId;
-  
+
           // DB에 메시지 저장
           await insertMessagesRecord({
             roomId: roomId,
@@ -388,7 +328,7 @@ const attachSocketEvents = (io) => {
             message: message,
             timestamp: new Date().toISOString(),
           });
-  
+
           // 방에 속한 사용자들에게 메시지 전송
           io.to(roomId).emit("newMessage", {
             userId,
@@ -405,25 +345,7 @@ const attachSocketEvents = (io) => {
         socket.emit("messageError", { message: "User authentication failed" });
       }
     });
-  
-    socket.on("joinRoom", async (data) => {
-      const { roomId } = data;
-      socket.join(roomId);
-    
-      // DB에서 해당 방의 이전 메시지를 불러옴
-      const messages = await loadMessages(roomId);
-    
-      // 클라이언트에 메시지 전송
-      socket.emit("loadMessages", messages);
-    });
   });
-}
-  
+};
 
-// 프로그램 종료 시 MySQL 연결 종료
-process.on("SIGINT", async () => {
-  await pool.end();
-  process.exit();
-});
-
-export { router, attachSocketEvents }
+export { router, attachSocketEvents };
