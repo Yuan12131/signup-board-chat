453a4a8e160e46d5814e7abc6f6000a7788f7c41
leeyuan <dbdks32123@gmail.com>
2023-12-05 12:54:00 +0900

    port 추가

--- database/config.mjs
+++ database/config.mjs
@@ -3,8 +3,9 @@ import mysql2 from "mysql2/promise"
 const dbConfig = {
   host: "localhost",
   user: "lee",
-  password: "kdt",
+  password: "1234",
   database: "community",
+  port: 3306, // MariaDB의 포트 번호 (기본값은 3306)
 };
 
 // 데이터베이스 연결을 관리하기 위한 데이터베이스 풀을 생성하는 메서드
